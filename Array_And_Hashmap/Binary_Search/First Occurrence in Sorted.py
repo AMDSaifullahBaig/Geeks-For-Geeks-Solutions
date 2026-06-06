594ae9011e12a7e016f088a1f4ab4ebf410705d0
A.MD Saifullah Baig <mdsaifu2007@gmail.com>
2026-06-06 21:14:42 +0530

    Solved First Occurrence in Sorted

---
+++ Array_And_Hashmap/Binary_Search/First Occurrence in Sorted.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstSearch(self, arr, k):
+        l=0
+        r=len(arr)-1
+        result=-1
+        while l<=r:
+            m=(l+r)//2
+            if arr[m]==k:
+                result=m
+                r=m-1
+            elif arr[m]>k:r=m-1
+            else:l=m+1
+        return result
